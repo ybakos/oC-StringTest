497c93c6ea6f82269f872381ae55d80bf568a5a1
Yong Bakos <ybakos@humanoriented.com>
2011-11-27 15:56:34 -0700

    Initial Commit

---
+++ StringTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  StringTest
+//
+//  Created by Yong Bakos on 11/27/11.
+//  Copyright (c) 2011 Human-Oriented Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
