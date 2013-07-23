cbd9bd956b89a075f8c1f48adbedf2416c9bdcf0
Yong Bakos <ybakos@humanoriented.com>
2013-07-22 19:14:00 -0600

    Adding simple cin and cout implementations.

--- StringTest/main.m
+++ StringTest/main.m
@@ -8,15 +8,55 @@
 
 #import <Foundation/Foundation.h>
 
-int main (int argc, const char * argv[])
-{
+/*
+ * Chomp the newline character
+ */
+@interface NSString (chomp)
+-(NSString *) chomp;
+@end
+@implementation NSString (chomp)
+-(NSString *) chomp {
+    return [self substringToIndex:[self length] - 1];
+}
+@end
 
+
+NSString* cin(void);
+void cout(NSString*);
+
+int main (int argc, const char * argv[]) {
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+        NSString* text = cin();
+        text = [text chomp];
+        cout(text);
     }
     return 0;
 }
 
+
+/*
+ *  Reading a string from stdin
+ */
+NSString* cin(void) {
+    @autoreleasepool {
+        // Instantiate NSData object using data from stdin
+        NSData* input = [[NSFileHandle fileHandleWithStandardInput] availableData];
+        // Initialize NSString with NSData
+        NSString *name = [[NSString alloc] initWithData:input encoding:NSUTF8StringEncoding];
+        return name;
+    }
+}
+
+/*
+ *  Writing a string to stdout.
+ */
+void cout(NSString* s) {
+    @autoreleasepool {
+        // Convert NSString to NSData
+        NSData* outData = [s dataUsingEncoding:NSUTF8StringEncoding];
+        // Instantiate an NSFileHandle whose target is stdout
+        NSFileHandle* output = [NSFileHandle fileHandleWithStandardOutput];
+        
+        [output writeData: outData];
+    }
+}
